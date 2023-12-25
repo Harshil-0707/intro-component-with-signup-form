1609c8f15c9f7e0f54ee6bc8c994ad72756d28ff
Harshil-0707 <chaurasiyaharshil7705@gmail.com>
2023-12-25 18:00:19 +0530

    changed some styles and logic

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-document.getElementById('btn').onclick = () => {
+const emailFormat = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    (document.getElementById('firstName').value)
-        ?   full('firstName')
-        :   empty('firstName');
+document.getElementById('btn').onclick = () =>{
+    const input = document.querySelectorAll("input");
+    for(let i = 0;i < input.length;i++){
+        if(!input[i].value){
+            input[i].classList.add("error");
+            empty(input[i].id);
+        }else{
+            input[i].classList.remove("error");
+            full(input[i].id);
+        }
 
-    (document.getElementById('lastName').value)
-        ?   full('lastName')
-        :   empty('lastName');
-    
-    (document.getElementById('Email').value)
-        ?   full('Email')
-        :   empty('Email');
-    
-    (document.getElementById('Password').value)
-        ?   full('Password')
-        :   empty('Password');
-}
+        if(i == 2 && !input[2].value.match(emailFormat)){
+            input[i].classList.add("error");
+            empty(input[i].id);
+        }
+    }
+};
 
-function empty(inputName){
-    document.getElementById(inputName).nextElementSibling.style.color = "hsl(0, 100%, 74%)";
-    document.getElementById(inputName).style.borderColor = "hsl(0, 100%, 74%)";    
+function empty(inputName){ 
+    document.getElementById(inputName).style.borderColor = "hsl(0, 100%, 74%)";
+    document.getElementById(inputName).nextElementSibling.style.display = "block";
 }
 
 function full(inputName){
-    document.getElementById(inputName).nextElementSibling.style.color = "#fff";
+    document.getElementById(inputName).nextElementSibling.style.display = "none";
     document.getElementById(inputName).style.borderColor = "hsl(246, 25%, 77%)";
 }
